3e385149b2cd96c1efd84358d8eef68a219fe830
Gbambor-Jnr <54485277+Gbambor-Jnr@users.noreply.github.com>
2023-04-30 11:33:50 +0200

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,10 +1,6 @@
 export const MONGO_URI =
   "mongodb+srv://ikenna:Cornelik@cluster0.kytwgsm.mongodb.net/online-food-tsc";
 
-export const APP_SECRET = "IkennaIsAStrongMan ";
 
-export const SEND_GRID_KEY =
-  "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";
 
-export const TRANS_USER = "gbambor@gmail.com";
-export const TRANS_PASS = "Cornelik23232323";
+
