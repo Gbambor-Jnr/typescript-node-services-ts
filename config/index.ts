1fe9702adb4895f42be914b8bea3fabac717c0f7
Okeke Ikenna <corneliusikennaokeke@gmail.com>
2023-05-01 18:15:37 +0200

    third commit

--- config/index.ts
+++ config/index.ts
@@ -1,14 +1,6 @@
 export const MONGO_URI =
   "mongodb+srv://ikenna:Cornelik@cluster0.kytwgsm.mongodb.net/online-food-tsc";
 
-export const APP_SECRET = "IkennaIsAStrongMan ";
-
-export const SEND_GRID_KEY =
-  "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";
-
-export const TRANS_USER = "gbambor@gmail.com";
-export const TRANS_PASS = "Cornelik23232323";
-
 export const TWILO_SID = ""; //gotten from the twilio website
 export const TWILO_AUTH_TOKEN = ""; // from twillio website
 export const TWILO_PHONE = ""; // from twillio website
